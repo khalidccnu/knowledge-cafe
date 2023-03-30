2a396f1346571de67894df4af2e41e5c46b51629
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-30 21:01:34 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        "inter": ["Inter", ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#4B6BFB",
+          "secondary": "#7B92B2",
+          "accent": "#67CBA0",
+          "neutral": "#181A2A",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
