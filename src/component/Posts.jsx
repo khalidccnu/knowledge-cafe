42fa5e175fd1faa82e226b5cdfe3aa5fd5e86872
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-03-31 04:01:58 +0600

    show all functionality added

--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -3,15 +3,25 @@ import Post from "./Post.jsx";
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(_ => {
         fetch("./posts.json").then(r => r.json()).then(data => setPosts(data));
     }, []);
 
     return (
-        <div className="grid grid-cols-1 gap-5">
+        <div>
+            <div className="grid grid-cols-1 gap-5">
+                {
+                    posts.slice(0, showAll ? posts.length : 3).map(post => <Post key={post.id} post={post} />)
+                }
+            </div>
             {
-                posts.map(post => <Post key={post.id} post={post} />)
+                !showAll ? (
+                    <div className="mt-6 text-center">
+                        <button type="button" className="btn btn-sm btn-primary" onClick={_ => setShowAll(true)}>Show All</button>
+                    </div>
+                ) : null
             }
         </div>
     );
